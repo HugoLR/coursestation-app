54b34313f1affdc58d1fdf66b3f19d7993c58625
Hugo Luna <hugo94ronal@gmail.com>
2019-02-17 11:45:44 -0600

    Main page styes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-// import Form from "react-bootstrap/Form"
-// import Button from "react-bootstrap/Button"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
+import Container from "react-bootstrap/Container"
+
+import "./home.css"
 
 class Home extends Component {
   constructor(props) {
@@ -24,13 +25,23 @@ class Home extends Component {
   render() {
     console.log(this.props)
     return (
-      <React.Fragment>
-      
-        <label>
-          <input type="text" value={this.state.newquery} onChange={this.handleChange} />
-        </label>
-        <Link to="/courses"><button onClick={() => this.props.onChangeValue(this.state.newquery)}>Search</button></Link>
-      </React.Fragment>
+      <Container className="home-container">
+        <video className="background-video" loop autoPlay>
+          <source src={require('../../video/MilkyWay1H264.mov')} type="video/mp4" />
+        </video>
+        <div className="home-container-text">
+          <div>
+            <i class="fas fa-space-shuttle"></i><span className="main-menu-logo-title">Course Station</span>
+          </div>
+          <div>
+            <h3>All courses In On Place</h3>
+          </div>
+          <div>
+            <input type="text" value={this.state.newquery} onChange={this.handleChange} />
+            <Link to="/courses"><button onClick={() => this.props.onChangeValue(this.state.newquery)}>Search</button></Link>
+          </div>
+        </div>
+      </Container>
     );
   }
 }
--- src/components/navbar/Navigationbar.js
+++ src/components/navbar/Navigationbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
-// import Brand from 'react-bootstrap/Navbar'
+import Dropdown from 'react-bootstrap/Dropdown'
 import "./nabvar.css"
 
 class Navigationbar extends Component {
@@ -17,23 +17,31 @@ class Navigationbar extends Component {
   render() {
     return(
         <Navbar className="main--menu" expand="lg">
-          <Navbar.Brand>
-            <Link to="/"><img src={require('../../images/logo.png')} width={190} height={70} alt="Logo"/></Link>
+          <Navbar.Brand className="main--menu--elements">
+            <Link to="/"><i class="fas fa-space-shuttle"></i><span className="main-menu-logo-title">Course Station</span></Link>
           </Navbar.Brand>
           <Navbar.Brand>
-            <Link to="/about">About</Link>
+            <Link className="main-menu-link" to="/about">About</Link>
           </Navbar.Brand>
           <Navbar.Brand>
-            <Link to="/login">Login</Link>
+            <Link className="main-menu-link" to="/about">Blog</Link>
           </Navbar.Brand>
           <Navbar.Brand>
-            <Link to="/signup">Signup</Link>
+            <Link className="main-menu-link" to="/about">Top</Link>
           </Navbar.Brand>
-          <Navbar.Brand>
-            <button onClick={ this.handleLogout }>Logout</button>
-          </Navbar.Brand>
-          <Navbar.Brand className="icon--menu"><i className="fas fa-user-astronaut"></i></Navbar.Brand>
-        </Navbar>
+          <Navbar.Brand className="icon--menu">
+            <Dropdown>
+              <Dropdown.Toggle split className="main-menu-dropdown">
+                <i className="fas fa-user-astronaut"></i>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item><Link className="second-menu-link" to="/login">Login</Link></Dropdown.Item>
+                <Dropdown.Item><Link className="second-menu-link" to="/signup">Signup</Link></Dropdown.Item>
+                <Dropdown.Item><button  className="second-menu-link main-menu-button" onClick={ this.handleLogout }>Logout</button></Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>;
+        </Navbar.Brand>
+      </Navbar>
       )
     }
 }
